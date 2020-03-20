891fda9d5fc7e5904cb5e6f43d230b9064dc06c1
Marcin Woźniak <y0rune@aol.com>
2020-03-20 13:28:24 +0100

    Updated

--- config.h
+++ config.h
@@ -1,4 +1,3 @@
-/* Last Update: 25-01-2020 */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -24,14 +23,16 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-    { "qutebrowser",             NULL,       NULL,        1 << 1,     False,       0 },
+    { "Gimp",                NULL,       NULL,        0,          True,       -1 },
+    { "pulsemixer",          NULL,       NULL,        0,          True,       -1 },
+    { "qutebrowser",         NULL,       NULL,        1 << 1,     False,       0 },
     { "Firefox",  			 NULL,       NULL,        1 << 1,     False,       0 },
 	{ "Thunderbird",         NULL,       NULL,        1 << 2,     False,       0 },
 	{ "Steam",         		 NULL,       NULL,        1 << 3,     False,       0 },
-	{ "Lutris",         	 NULL,       NULL,        1 << 3,     False,       0 },	
-	{ "League of Legends", 	 NULL,       NULL,        1 << 3,     True,        0 },	
-	{ "TeamSpeak 3",       	 NULL,       NULL,        1 << 3,     False,       0 },	
-	{ "discord",           	 NULL,       NULL,        1 << 4,     False,       0 },	
+	{ "Lutris",         	 NULL,       NULL,        1 << 3,     False,       0 },
+	{ "League of Legends", 	 NULL,       NULL,        1 << 3,     True,        0 },
+	{ "TeamSpeak 3",       	 NULL,       NULL,        1 << 3,     False,       0 },
+	{ "discord",           	 NULL,       NULL,        1 << 4,     False,       0 },
     { "KeePassXC",           NULL,       NULL,        1 << 7,     False,       0 },
 	{ "TeamViewer",    		 NULL,       NULL,        1 << 8,     False,       0 },
     { "obs",         		 NULL,       NULL,        1 << 8,     False,       0 },
