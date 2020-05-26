d4ef2299656fa45f2259a3b5e54349614783ab15
Marcin Woźniak <y0rune@aol.com>
2020-05-27 00:04:18 +0200

    Added gaps and border
    Signed-off-by: Marcin Wozniak <y0rune@aol.com>

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 25;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
