6d119dd12fa74a9ac3bae66f3de72baf4fb67f63
Marcin Woźniak <y0rune@aol.com>
2021-01-06 23:08:57 +0100

    Updated dwm
    Signed-off-by: Marcin Woźniak <y0rune@aol.com>

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 25;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -25,18 +25,15 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
     { "Gkrellm",             NULL,       NULL,        1 << 0,     True,        0 },
     { "Gimp",                NULL,       NULL,        0,          True,        0 },
-    { "mpv",                 NULL,       NULL,        0,          True,        0 },
     { NULL,                  NULL, "pulsemixer",      0,          True,        0 },
+    { "mpv",                 NULL,       NULL,        0,          True,        0 },
+    { "MuPDF",               NULL,       NULL,        0,          True,        0 },
     { "Brave",               NULL,       NULL,        1 << 1,     False,       0 },
     { "Firefox",  			 NULL,       NULL,        1 << 1,     False,       0 },
-	{ "Thunderbird",         NULL,       NULL,        1 << 2,     False,       0 },
-	{ "Steam",         		 NULL,       NULL,        1 << 3,     False,       0 },
-    { "Lutris",         	 NULL,       NULL,        1 << 3,     False,       0 },
-	{ "TeamSpeak 3",       	 NULL,       NULL,        1 << 3,     False,       0 },
-	{ "discord",           	 NULL,       NULL,        1 << 4,     False,       0 },
-    /*{ "KeePassXC",           NULL,       NULL,        1 << 7,     False,       0 },*/
-    { "TeamViewer",    		 NULL,       NULL,        1 << 8,     False,       0 },
-    { "obs",         		 NULL,       NULL,        1 << 8,     False,       0 },
+    { "FreeRDP",  			 NULL,       NULL,        1 << 2,     False,       0 },
+    { "Teams",               NULL,       NULL,        1 << 4,     False,       0 },
+    { "Spicy",  			 NULL,       NULL,        1 << 2,     False,       0 },
+
 };
 
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -74,7 +71,7 @@ static const char *cmusplay[]  = { "cmus-remote", "-u", NULL };
 static const char *cmusnext[]  = { "cmus-remote", "-n", NULL };
 static const char *cmusprev[]  = { "cmus-remote", "-r", NULL };
 static const char *shutdownpress[] = {"shut-sup-rest", "NULL" };
-static const char *screenswitcher[] = { "screen-switcher", "NULL" }; 
+static const char *screenswitcher[] = { "screen-switcher", "NULL" };
 
 static Key keys[] = {
 	{0,				         0x1008ff02,		spawn, 	        SHCMD ("sudo light -A 10")},
@@ -85,14 +82,14 @@ static Key keys[] = {
 	{ MODKEY,		         XK_F2,	   	    	spawn, 		    SHCMD ("ponymix decrease 5")},
     { MODKEY,		         XK_F1,	   	    	spawn,		    SHCMD ("ponymix toggle")},
 	{ MODKEY|ShiftMask,	     XK_F2,  	   		spawn,		    SHCMD ("cmus-remote -v -5%")},
-	{ MODKEY|ShiftMask,      XK_F3,	       		spawn,		    SHCMD ("cmus-remote -v +5%")},	
+	{ MODKEY|ShiftMask,      XK_F3,	       		spawn,		    SHCMD ("cmus-remote -v +5%")},
 	{ MODKEY,		         XK_F3,	   	    	spawn,		    SHCMD ("ponymix increase 5")},
 	{ MODKEY, 		         XK_F10,		    spawn,		    {.v = screenswitcher} },
 	{ MODKEY, 		         XK_F11,		    spawn,		    SHCMD ("sudo light -U 10")},
 	{ MODKEY, 		         XK_F12,		    spawn,		    SHCMD ("sudo light -A 10")},
     {0,                      XK_Print,  	    spawn,          {.v = print_screen_cmd } },
     { MODKEY,                XK_Print,  	    spawn,          {.v = print_screen_cmd_area } },
-    { MODKEY,		         XK_l,	   		    spawn,	   	    {.v = cmdlock } },		
+    { MODKEY,		         XK_l,	   		    spawn,	   	    {.v = cmdlock } },
 	{ MODKEY,                XK_d,      		spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,      XK_Return, 		spawn,          {.v = termcmd } },
 	{ MODKEY,                XK_b,      		togglebar,      {0} },
@@ -126,8 +123,8 @@ static Key keys[] = {
     { 0,             		 0x1008ff17,       	spawn,          {.v = cmusnext }},
     { 0,             	     0x1008ff16,        spawn,          {.v = cmusprev }},
     /*{ MODKEY,                       XK_t,     		setlayout,      {.v = &layouts[0]} },*/
-	/*{ MODKEY,                       XK_f,      		setlayout,      {.v = &layouts[2]} },*/ 
-	/* { MODKEY|ShiftMask,             XK_q,      		spawn,          {.v = dwmkill } }, */ 
+	/*{ MODKEY,                       XK_f,      		setlayout,      {.v = &layouts[2]} },*/
+	/* { MODKEY|ShiftMask,             XK_q,      		spawn,          {.v = dwmkill } }, */
 	/*{ MODKEY,                       XK_space,  		setlayout,      {0} },*/
 	/*{ MODKEY|ShiftMask,      XK_f,      		setlayout,      {.v = &layouts[1]} },*/
     TAGKEYS(                        XK_1,                      0)
@@ -157,4 +154,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
